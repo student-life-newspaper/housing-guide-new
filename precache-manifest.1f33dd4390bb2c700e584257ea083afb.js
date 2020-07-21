8fcb5cde445ce3adbc35c7a0f7310e5bb4057484
Nathan Springman <naspringman@gmail.com>
2020-07-21 17:40:19 -0500

    Updates

---
+++ precache-manifest.1f33dd4390bb2c700e584257ea083afb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266d1615d2422c57afb24d437cff2a7b",
+    "url": "/student-life-newspaper/housing-guide-new/index.html"
+  },
+  {
+    "revision": "e2591c215e8e3540b5ce",
+    "url": "/student-life-newspaper/housing-guide-new/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f89061c39ef7c6c754f3",
+    "url": "/student-life-newspaper/housing-guide-new/static/css/main.7b0a67ba.chunk.css"
+  },
+  {
+    "revision": "e2591c215e8e3540b5ce",
+    "url": "/student-life-newspaper/housing-guide-new/static/js/2.7484155d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/student-life-newspaper/housing-guide-new/static/js/2.7484155d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89061c39ef7c6c754f3",
+    "url": "/student-life-newspaper/housing-guide-new/static/js/main.64ef7a04.chunk.js"
+  },
+  {
+    "revision": "a17d3303f68bba82a96f",
+    "url": "/student-life-newspaper/housing-guide-new/static/js/runtime-main.d2b14400.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/student-life-newspaper/housing-guide-new/static/media/logo.5d5d9eef.svg"
+  }
+]);
