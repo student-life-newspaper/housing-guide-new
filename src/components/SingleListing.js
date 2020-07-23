01dd4c03d434e1fbcd9ca5597a3fe294c0c65c89
Nathan Springman <naspringman@gmail.com>
2020-07-22 22:37:42 -0500

    make dorm listings tabbable, add todo

--- src/components/SingleListing.js
+++ src/components/SingleListing.js
@@ -13,9 +13,11 @@ export default class SingleListing extends React.Component {
   }
   
   render(){
+    // TODO: onKeyDown={return event.keyCode != 13 || this.selectDorm}
+    // add enter key on dorm selection
     let dorm = this.props.dorm;
     return(
-        <Card className='single-listing cursor-pointer w-100 mb-3' onClick={this.selectDorm}>
+        <Card className='single-listing cursor-pointer w-100 mb-3' tabIndex="0" onClick={this.selectDorm}>
           <Row noGutters>
             <Col md='4'>
               <img className='dorm-thumbnail' src={process.env.PUBLIC_URL + '/img/thumbs/' + this.props.dorm.shortName + '.png'} alt={dorm.shortName}/>
