50c9d26b44161637b76ef8fff3e85f5935bd0357
Nathan Springman <naspringman@gmail.com>
2020-07-28 17:19:22 -0500

    fix: map margins

--- src/components/ModalMap.js
+++ src/components/ModalMap.js
@@ -13,6 +13,7 @@ class ModalMap extends React.Component {
        center={position} 
        zoom={18} 
        style={{ width: '100%', height: '100%'}}
+       className="mx-3"
       >
         <TileLayer
            attribution={'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'}
