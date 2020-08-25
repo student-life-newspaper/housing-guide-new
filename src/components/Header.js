1d4580336e7c812e2702cef2aedfcefe084edbf5
Nathan Springman <naspringman@gmail.com>
2020-08-24 20:32:38 -0500

    fix: add banner link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ function Header(props) {
   return (
     <div id="housing-header" className="mb-4">
       <div className="banner-container my-3">
-        <img className="off-campus-banner" alt="Off Campus Living" src={`${process.env.PUBLIC_URL}/img/banner.gif`}/>
+        <a href="https://www.studlife.com/off-campus-living/">
+          <img className="off-campus-banner" alt="Off Campus Living" src={`${process.env.PUBLIC_URL}/img/banner.gif`}/>
+        </a>
       </div>
       <h1>Welcome to Student Life’s Washington University Housing&nbsp;Guide</h1>
       <p>Curious what your first freshman residence hall will look like? Can’t figure out where you want to live next year? We’ve got you covered with our Housing Guide for all Residential Life dorms and apartments. Each page includes photos of the rooms and building, a list of pros and cons, pricing, estimated walk times and a summary. Please note: photographs are meant to be a representative sample of the dorm or apartment; there may be major variance between different rooms in the same building.</p>
